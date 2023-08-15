e338f990f9f8bcc28659ec4bc3f3fafe70ace0fb
ymew <yeon3191225@gmail.com>
2023-08-15 18:24:37 +0900

    23.08.15

---
+++ fish/src/test/java/com/yeonmew/fish/FishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonmew.fish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
